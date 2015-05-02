11088fdcd44c79c5098026586433ad97f78f81c9
Chris Amanse <chrisamanse@AC1234.charter.com>
2015-05-02 14:43:54 -0700

    Initial Commit

---
+++ CircularProgressView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircularProgressView
+//
+//  Created by Chris Amanse on 5/2/15.
+//  Copyright (c) 2015 Joe Christopher Paul Amanse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
